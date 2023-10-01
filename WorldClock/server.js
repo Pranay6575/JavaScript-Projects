aaa7d5e3373be8ed1b976dfa2eeb4b6b1fd1e7d6
Pranay <pranaypatil6575@gmail.com>
2023-10-01 19:28:50 +0530

    country sections added

--- WorldClock/server.js
+++ WorldClock/server.js
@@ -10,13 +10,23 @@
 //   console.log(`Example app listening on port ${port}`)
 // })
 
-let d = new Date()
-usa.innerHTML = d.toLocaleString('en-US', {
-    timeZone: 'America/Los_Angeles',
-  }).split(", ")[1]
-india.innerHTML = d.toLocaleString('en-US', {
-    timeZone: 'America/Los_Angeles',
-  }).split(", ")[1]
-china.innerHTML = d.toLocaleString('en-US', {
-    timeZone: 'America/Los_Angeles',
-  }).split(", ")[1]
+let updateTime = () => {
+  let d = new Date();
+  usa.innerHTML = d
+    .toLocaleString("en-US", {
+      timeZone: "America/Los_Angeles",
+    })
+    .split(", ")[1];
+  china.innerHTML = d
+    .toLocaleString("en-US", {
+      timeZone: "Asia/Shanghai",
+    })
+    .split(", ")[1];
+  india.innerHTML = d
+    .toLocaleString("en-US", {
+      timeZone: "Asia/Kolkata",
+    })
+    .split(", ")[1];
+};
+
+setInterval(updateTime, 1000);
